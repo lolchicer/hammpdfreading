51f93502b883cfa29f2c976c77778d4ea48040c7
lolchicer <lolchicer@live.ru>
2023-03-17 17:48:18 +0300

    путь к базе...

--- Test03/Program.cs
+++ Test03/Program.cs
@@ -6,7 +6,7 @@ using Infor.HammPdfReading.Sqlite;
 Console.WriteLine("строка:");
 Detail detail = (Detail)Console.ReadLine();
 
-Console.WriteLine("путь базы:");
+Console.WriteLine("путь к базе:");
 var path = Console.ReadLine();
 
 Builder.Build(path);
