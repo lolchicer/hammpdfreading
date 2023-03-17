d91facd3a90cfa1ffcc98321db222eedc1940e9f
lolchicer <lolchicer@live.ru>
2023-03-17 17:14:24 +0300

    Builder.Build(path)

---
+++ HammPdfReading.Sqlite/Builder.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.Sqlite;
+using Infor.HammPdfReading;
+
+namespace Infor.HammPdfReading.Sqlite {
+    public static class Builder {
+        public static void Build(string path) {
+            var connection = new SqliteConnection($"DataSource={path}");
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = "" +
+                "CREATE TABLE details (" +
+                "   item REAL NOT NULL," +
+                "   part_no INTEGER PRIMARY KEY," +
+                "   valid_for_1 INTEGER NOT NULL," +
+                "   valid_for_2 INTEGER NOT NULL," +
+                "   quantity INTEGER NOT NULL," +
+                "   unit INTEGER NOT NULL," +
+                "   designation TEXT NOT NULL" +
+                ")";
+            command.ExecuteNonQuery();
+        }
+    }
+}
---
+++ Test03/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Infor.HammPdfReading;
+using Infor.HammPdfReading.Sqlite;
+
+Console.WriteLine("путь базы:");
+Builder.Build(Console.ReadLine());
