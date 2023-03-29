8cf2de670800fba3ef00672e091abb0531dfed00
lolchicer <lolchicer@live.ru>
2023-03-29 15:55:30 +0300

    Assembly.cs (orm moment)

---
+++ HammPdfReading/Assembly.cs
@@ -0,0 +1,10 @@
+﻿namespace Infor.HammPdfReading
+{
+    public struct Assembly
+    {
+        public int No { get; set; }
+        public string Designation { get; set; }
+
+        public string ToString() => $"{No} {Designation}";
+    }
+}
