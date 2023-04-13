9edcdae38371d08f86d9b180edbdfcf8a36157d2
lolchicer <lolchicer@live.ru>
2023-04-13 13:23:02 +0300

    конвертация в csv

---
+++ HammPdfReading.Csv/Builder.cs
@@ -0,0 +1,43 @@
+﻿using CsvHelper;
+using System.Globalization;
+using System.Text;
+
+namespace Infor.HammPdfReading.Csv
+{
+    public class Builder
+    {
+        string _path;
+
+        public void Build()
+        {
+            using (FileStream stream = File.Create(_path)) ;
+        }
+
+        public void Insert(ExtendedDetail detail)
+        {
+            using (StreamWriter writer = new StreamWriter(_path, true, Encoding.UTF8))
+            {
+                using (CsvWriter csvWriter = new CsvWriter(writer, new CultureInfo("ru-RU", false)))
+                {
+                    csvWriter.WriteRecord(detail);
+                }
+            }
+        }
+
+        public void Insert(ExtendedDetail[] details)
+        {
+            using (StreamWriter writer = new StreamWriter(_path, true, Encoding.UTF8))
+            {
+                using (CsvWriter csvWriter = new CsvWriter(writer, new CultureInfo("ru-RU", false)))
+                {
+                    csvWriter.WriteRecords(details);
+                }
+            }
+        }
+
+        public Builder (string path)
+        {
+            _path = path;
+        }
+    }
+}
---
+++ Test06/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using iTextSharp.text.pdf;
+using Infor.HammPdfReading;
+using Infor.HammPdfReading.Csv;
+
+Console.WriteLine("путь к документу:");
+var reader = new Reader(new PdfReader(Console.ReadLine()));
+var details = reader.ExtendedDetails(45, 10);
+
+Console.WriteLine("путь к базе:");
+var path = Console.ReadLine();
+
+var builder = new Builder(path);
+builder.Build();
+builder.Insert(details.ToArray());
