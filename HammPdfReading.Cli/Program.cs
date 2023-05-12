d538f64f564ce35843583e691ee8d1a4c07a0b3a
lolchicer <lolchicer@live.ru>
2023-05-13 01:14:23 +0300

    HammPdfReading.Cli

---
+++ HammPdfReading.Cli/Program.cs
@@ -0,0 +1,58 @@
+﻿using iTextSharp.text.pdf;
+using Infor.HammPdfReading;
+using Infor.HammPdfReading.Csv;
+using System.IO;
+
+// вывод
+var output = string.Empty;
+
+// распаршенные аргументы/нуллеваемость для вывода
+string? pdfPath = null;
+string databasePath = Path.GetDirectoryName(
+    System.Reflection.Assembly.GetExecutingAssembly().Location) +
+    "\\hamm.csv" ??
+    Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+// число лишних аргументов/нуллеваемость для вывода
+int excessiveArgsCount = 0;
+
+// лямбда-выражения для парсинга аргументов
+Action<string>[] validations = {
+    (arg) => pdfPath = arg, 
+    (arg) => databasePath = arg};
+
+// парсинг аргументов
+for (int i = 0; i < args.Length; i++)
+    if (args[i] != null)
+        validations[i](args[i]);
+    else
+        excessiveArgsCount++;
+
+// запись pdf-файлов
+if (pdfPath != null)
+{
+    var builder = new Builder(databasePath);
+
+    if (!File.Exists(databasePath))
+        builder.Build();
+
+    foreach (var file in Directory.GetFiles(pdfPath))
+    {
+        var pdfReader = new PdfReader(file);
+        var reader = new Reader(pdfReader);
+
+        await Task.Run(() => builder.Join(reader.ExtendedDetails(), reader.GetModules()));
+        Console.WriteLine($"Содержимое {file} выведено в {databasePath}.\n");
+    }
+}
+
+// сборка сообщения для вывода
+
+if (pdfPath == null)
+    output += "Не указан путь к PDF-файлам.\n";
+
+if (excessiveArgsCount > 0)
+    output += $"Лишних аргументов: {excessiveArgsCount}.\n";
+
+// вывод
+Console.Write(output);
