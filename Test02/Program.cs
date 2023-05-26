f168be6a55926025ddd96566adcf74b4c58c9759
lolchicer <lolchicer@live.ru>
2023-05-26 06:50:04 +0300

    вывод текста Test02 в комментарий, так функционала, вызываемого оттуда, в HammPdfReading больше нет

--- Test02/Program.cs
+++ Test02/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Infor.HammPdfReading;
 
-var detail = Console.ReadLine();
-Console.WriteLine(HammPdfReader.Details(detail));
+/* var detail = Console.ReadLine();
+Console.WriteLine(HammPdfReader.Details(detail)); */
