645f9a9913378977b20885086e968387c8528794
lolchicer <lolchicer@live.ru>
2023-06-13 03:14:31 +0300

    Запись изображений в HammPdfWriter

--- HammPdfReading.Csv/HammPdfWriter.cs
+++ HammPdfReading.Csv/HammPdfWriter.cs
@@ -8,15 +8,17 @@ namespace Infor.HammPdfReading.Csv
     {
         string _path;
 
+        string Path => System.IO.Path.Combine(_path, "table.csv");
+
         public void Build()
         {
-            using (FileStream stream = File.Create(_path)) ;
+            using (FileStream stream = File.Create(Path)) ;
             JoinHeader();
         }
 
         public void Insert(ExtendedDetail detail)
         {
-            using (StreamWriter writer = new StreamWriter(_path, true, Encoding.UTF8))
+            using (StreamWriter writer = new StreamWriter(Path, true, Encoding.UTF8))
             {
                 using (CsvWriter csvWriter = new CsvWriter(writer, new CultureInfo("ru-RU", false)))
                 {
@@ -27,7 +29,7 @@ namespace Infor.HammPdfReading.Csv
 
         public void Insert(ExtendedDetail[] details)
         {
-            using (StreamWriter writer = new StreamWriter(_path, true, Encoding.UTF8))
+            using (StreamWriter writer = new StreamWriter(Path, true, Encoding.UTF8))
             {
                 using (CsvWriter csvWriter = new CsvWriter(writer, new CultureInfo("ru-RU", false)))
                 {
@@ -38,7 +40,7 @@ namespace Infor.HammPdfReading.Csv
 
         public void JoinHeader()
         {
-            using (StreamWriter writer = new StreamWriter(_path, true, Encoding.UTF8))
+            using (StreamWriter writer = new StreamWriter(Path, true, Encoding.UTF8))
             {
                 using (CsvWriter csvWriter = new CsvWriter(writer, new CultureInfo("ru-RU", false)))
                 {
@@ -63,11 +65,12 @@ namespace Infor.HammPdfReading.Csv
 
         public void Join(ExtendedDetail[] details, Module[] modules)
         {
-            using (StreamWriter writer = new StreamWriter(_path, true, Encoding.UTF8))
+            using (StreamWriter writer = new StreamWriter(Path, true, Encoding.UTF8))
             {
                 using (CsvWriter csvWriter = new CsvWriter(writer, new CultureInfo("ru-RU", false)))
                 {
                     foreach (var module in modules)
+                    {
                         foreach (var detail in details)
                             if (detail.Assembly == module.No)
                             {
@@ -88,6 +91,11 @@ namespace Infor.HammPdfReading.Csv
                                     csvWriter.WriteField(field);
                                 csvWriter.NextRecord();
                             }
+                        var imageFolderPath = System.IO.Path.Combine(_path, module.No);
+                        Directory.CreateDirectory(imageFolderPath);
+                        for (int i = 0; i < module.Images.Length; i++)
+                            module.Images[i].Save(System.IO.Path.Combine(imageFolderPath, $"{i}.jpg"));
+                    }
                 }
             }
         }
