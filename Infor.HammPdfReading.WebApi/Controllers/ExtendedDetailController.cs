9119df11645139dfe622a5fb193e413d809e54d1
lolchicer <lolchicer@live.ru>
2023-06-21 11:13:36 +0300

    тестовое webapi

---
+++ Infor.HammPdfReading.WebApi/Controllers/ExtendedDetailController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using iTextSharp.text.pdf;
+
+namespace Infor.HammPdfReading.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExtendedDetailController : Controller
+    {
+        [HttpGet("{path}")]
+        public IEnumerable<ExtendedDetail> ExtendedDetails(string path)
+        {
+            var pdfReader = new PdfReader(path);
+            var reader = new HammPdfReader(pdfReader); 
+
+            return reader.GetExtendedDetails(40, 4);
+        }
+    }
+}
